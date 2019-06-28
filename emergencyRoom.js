5dff5d2c7a09b80b4fe298f6b103e237fe609d47
shahriya2p <shahriya2p@gmail.com>
2019-06-28 18:39:10 +0530

    initial commit

---
+++ emergencyRoom.js
@@ -0,0 +1,10 @@
+let emergencyRoom= {trauma:50, heartAttack:50}
+
+const fillBed = (type) => {
+	if(emergencyRoom[type]) {
+		emergencyRoom[type]--;
+		console.log(`Room successfully alloted\n total ${emergencyRoom[type]} room available for ${type} now`);
+	} else {
+		console.log('no room is available');
+	}
+}
